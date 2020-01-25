a939aa6ec25199bf62ff9923b48fc4d76bfb3016
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-01-25 13:31:39 +0800

    membuat konfigurasi di projek

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/phpmvc/13-search/public');
-
-// DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'phpmvc');
