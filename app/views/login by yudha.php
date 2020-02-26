f03d3e15ad2013071b168310c9cac5688d5de1f6
YudhaAd <adistarayudha@gmail.com>
2020-02-26 11:59:50 +0800

    membuat view login by yudha

---
+++ app/views/login by yudha.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login Page</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 loginimage bglogin"></div>
+
+				<div class="col-lg-6 loginpage">
+					<div class="container">
+						<div class="loginform">
+							<div class="logintulisan"><strong>User Login</strong></div>
+							<form action="">
+								<input type="text" name="username" class="form_login" placeholder="Username/Nik/Nis">
+								<input type="password" name="username" class="form_login" placeholder="Password">
+								<div class="dropdown">
+									<select name="" id="">
+										<option value="">Choose</option>
+										<option value="">Siswa</option>
+										<option value="">Guru</option>
+										<option value="">Admin</option>
+									</select>
+								</div>
+								<div class="button">
+									 <button type="submit">LOGIN</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
