30828e18cacf2fa58ae74728f496c0bcc5294614
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-03-09 13:41:12 +0800

    menggabungkan login yudha dengan backend

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -1,22 +1,41 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>login</title>
+	<meta charset="UTF-8">
+	<title>Login Page</title>
+	<link rel="stylesheet" href="<?= BASEURL; ?>/css_yudha/style.css">
 </head>
 <body>
-    <h1>LOGIN</h1>
-    <form action="<?= BASEURL ?>/auth/login" method="POST">
-        <input type="text" name="username" placeholder="nis / nip" required>
-        <input type="password" name="password" placeholder="password" required>
-        <select name="role" id="selectRole" required>
-            <option value="">-</option>
-            <option value="1">siswa</option>
-            <option value="2">guru</option>
-        </select>
-        <button type="submit">login</button>
-    </form>
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 loginimage bglogin"></div>
+
+				<div class="col-lg-6 loginpage">
+					<div class="container">
+						<div class="loginform">
+							<div class="logintulisan"><strong>User Login</strong></div>
+							<form action="<?= BASEURL ?>/auth/login" method="POST">
+								<input type="text" name="username" class="form_input" placeholder="Username/Nik/Nis">
+								<input type="password" name="password" class="form_input" placeholder="Password">
+								<div class="dropdown">
+									<select name="role" id="selectRole">
+										<option value="">Choose</option>
+										<option value="1">Siswa</option>
+										<option value="2">Guru</option>
+										<option value="3">Admin</option>
+									</select>
+								</div>
+								<div class="button">
+									 <button type="submit">LOGIN</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
