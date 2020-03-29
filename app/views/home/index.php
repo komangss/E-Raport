2cc5851c907760c25deb7d3d833870759924dc85
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-03-29 15:37:20 +0800

    membuat frontend home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,50 @@
-<h1>Hello <?= $data["nama"]; ?> </h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Siswa</title>
+    <link rel="stylesheet" href="<?= BASEURL_PUBLIC; ?>/css_anton/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <section class="navbar">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="bg-menu">
+                        <nav>
+                            <ul>
+                                <li class="logo"><a class="logoa" href="<?= BASEURL; ?>"><img src="http://localhost/E-Raport/public/image_anton/logo.png" width="150px"></a></li>
+                                <li><a  href="<?= BASEURL; ?>">testimony</a></li>
+                                <li><a  href="<?= BASEURL; ?>/auth">login</a></li>
+                                <li><a>Home</a></li>
+                                <li> <a href="javascript:void(0);" class="hide" onclick="myFunction()">
+                                        <img src="http://localhost/E-Raport/public/image_anton/icon-menu.png" width="30px">
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="banner">
+                        <div class="text-banner">
+                            <h1>Website E Raport</h1>
+                            <p>Disini Kami memberikan Segudang pengalaman dengan web ini.<br>
+                                Kalian bisa mengecek nilai dari Raport Kalian secara online<br>
+                                Melalui Website E Raport Ini.</p>
+                            <button class="btn-gradient">Get Started !</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+<script>
+
+</script>
+
+</html>
