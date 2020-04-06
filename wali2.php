4061157dcc83834debf62602d121d59c4845e429
evangelistaaa <60131485+Evangelistaaa@users.noreply.github.com>
2020-04-06 21:44:51 +0800

    Add files via upload

---
+++ wali2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>	Create Rapot</title>
+	<link rel="stylesheet" href="wali2.css">
+</head>
+<body>
+	<table width="100%">
+		<th class="header">
+      		<p> SMK N 1 Denpasar </p>
+  		</th>
+  		<tr>
+			<td class="body">
+      			<div class="table">
+        			<table width="100%">
+        <tr>
+            <td>
+              <p class="nama">Nama:</p>
+              <p class="data">Kelas:</p>
+              
+              <br>
+              <br>	
+              <p class="data">Mapel:</p>
+              <p class="data">Nilai Teori:</p>
+              <p class="data">Nilai Keterampilan:</p>
+              <p class="data">Nilai Sikap:</p>
+              <br>
+              <p class="data">Total:</p>
+              <br>
+              <br>	
+              <p class="data">Mapel:</p>
+              <p class="data">Nilai Teori:</p>
+              <p class="data">Nilai Keterampilan:</p>
+              <p class="data">Nilai Sikap:</p>
+              <br>
+              <br>	
+              <p class="data">Total:</p>
+           </td>
+        </tr>
+</body>
+</html>
