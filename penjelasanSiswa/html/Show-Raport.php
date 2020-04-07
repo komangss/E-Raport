f48d5d5c0be97e839087bb2f6e15e0f3194818fd
Bagusgede <agusyudistiawan02@gmail.com>
2020-04-07 11:28:53 +0800

    PenjelasanSiswa

---
+++ penjelasanSiswa/html/Show-Raport.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>showPage</title>
+</head>
+
+<body>
+    <div>
+        <div class="card box-shadow position-center uk-card">
+        <!-- tampilan nilai siswa -->
+            <h3 class="uk-card-title">Nilai</h3> 
+        </div>
+    </div>
+
+</body>
+
+</html>
