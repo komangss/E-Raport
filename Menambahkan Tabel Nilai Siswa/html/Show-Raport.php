cefc1b0d9641bca4ac6a798c6e0a7f415d8ed61a
YudhaAd <adistarayudha@gmail.com>
2020-04-08 09:53:06 +0800

    menambahkantabel

---
+++ Menambahkan Tabel Nilai Siswa/html/Show-Raport.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>showPage</title>
+</head>
+
+<body>
+    <div>
+        <div style="height:80%" class="card box-shadow position-center uk-card">
+            <!-- tampilan nilai siswa -->
+            <h3 class="uk-card-title">Nilai</h3>
+            <h2>Nama Siswa : </h2>
+            <h2>Kelas Siswa :</h2>
+            <h2>No Urut Siswa : </h2>
+            <br>
+            <hr>
+            <br>    
+
+            <table border="1" style="width:100%;" >
+		<tr>
+			<th>Mapel</th>
+			<th>KKM</th>
+			<th>NILAI</th>
+			
+		</tr>
+		<tr>
+			<td>Bahasa Indonesia</td>
+			<td>75</td>
+			<td>78</td>
+			
+		</tr>
+		<tr>
+			<td>Matematika</td>
+			<td>78</td>
+			<td>80</td>
+		
+		</tr>
+		<tr>
+			<td>Produk Kreatif dan Kewirausahaan</td>
+			<td>78</td>
+			<td>84</td>
+		</tr>
+		<tr>
+			<td>Perangkat Lunak</td>
+			<td>78</td>
+			<td>88</td>
+		</tr>
+		<tr>
+			<td>PWPB</td>
+			<td> 78</td>
+			<td>86</td>
+
+        </tr>
+        <tr>
+            <td>Bahasa Inggris</td>
+            <td>75</td>
+            <td>78</td>
+        </tr>
+        <tr>
+            <td colspan="2  ">Rata</td>
+            <td>89,6</td>
+        </tr>
+	</table>	
+        </div>
+    </div>
+
+</body>
+
+</html>
