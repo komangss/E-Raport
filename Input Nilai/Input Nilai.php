e02ee6a6948f5f031ea55ba8d14ee2191d92588c
dimasdesuu <60087543+dimasdesuu@users.noreply.github.com>
2020-04-08 08:48:26 +0700

    Memindahkan file kedalam folder

---
+++ Input Nilai/Input Nilai.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Input Nilai Siswa</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/uikit.min.css" />
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+</head>
+
+<body>
+
+
+
+    <div class="uk-grid-column-small uk-grid-row-large uk-child-width-1-3@s uk-text-left" uk-grid>
+        <div>
+            <div class="uk-card uk-card-default uk-card-body">
+                <div class="uk-padding">
+                    <h3 class="uk-card-title"> </h3>
+
+
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-stacked-select">Pilih kelas yang anda ajar</label>
+                        <div class="uk-form-controls">
+                            <select class="uk-select" id="form-stacked-select">
+                                <option>XI RPL 1</option>
+                                <option>XI RPL 2</option>
+                                <option>XI TKJ 1</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="uk-margin">
+                        <label class="uk-form-label" for="form-stacked-select">Pilih Murid</label>
+                        <div class="uk-form-controls">
+                            <select class="uk-select" id="form-stacked-select">
+                                <option>01 - Angga Tusan Aji</option>
+                                <option>02 - lkajdj</option>
+                                <option>03 - kasldawkj</option>
+                                <option>04 - ihsdahdsjdh</option>
+                                <option>05 - jskajsiueqw</option>
+                            </select>
+                        </div>
+
+
+
+                        <div class="uk-margin">
+                            <h5> Masukan Nilai
+                                <input class="uk-input" type="text" placeholder="Nilai Teori">
+                        </div>
+
+
+                        <div class="uk-margin">
+
+                            <input class="uk-input" type="text" placeholder="Nilai Keterampilan">
+
+                        </div>
+                        <div class="uk-margin">
+
+                            <input class="uk-input" type="text" placeholder="Nilai Sikap">
+
+                        </div>
+
+                        <button class="uk-button uk-button-default">CREATE</button>
+
+
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+                    <div>
+            <div class="uk-card uk-card-default uk-card-body"><div class="uk-padding">
+                    <h3 class="uk-card-title"> </h3>
+
+                    
+
+                    <p> <h4>Siswa :
+                        <br>
+                        
+                    <h4> Semester :
+
+                    <br>
+                     <h4> Nilai Keterampilan:
+                         <br>
+                         <h4>Nilai Teori :
+                         <br> <br>
+                        <h4> Nilai Sikap :
+                    
+
+
+
+
+
+
+                    
+
+                    
+
+
+
+
+
+                    </div>
+
+                </div></div>
+        </div>
+
+    </div>
+
+
+
+
+</body>
